cd5bb90c5032c50562ba95747b4becd1dc99849c
Serena Scalzi <serenascalzi@alumni.iu.edu>
2018-07-26 20:22:19 -0700

    built dynamic form with React

--- client/src/App.js
+++ client/src/App.js
@@ -19,11 +19,47 @@ class App extends Component {
     return (
       <div>
         <form>
+
         <h1>Sign Up For My Web App</h1>
-          {this.state.fields.map((field, i) => (
-            <input key={i} id={field.id} type={field.type} name={field.id} placeholder={field.label} />
-          ))}
+
+          {this.state.fields.map((field, i) => {
+            
+            if (field.type == 'text' || field.type == 'email' || field.type == 'tel') {
+              return (
+                <div className="input" key={`field-${i}`}>
+                  <i className={`fa ${field.icon}`}></i>
+                  <input id={field.id} type={field.type} placeholder={field.label} />
+                </div>
+              )
+            }
+
+            if (field.type == 'textarea') {
+              return (
+                <div className="txt" key={`field-${i}`}>
+                  <i className={`fa ${field.icon}`}></i>
+                  <textarea id={field.id} type={field.type} placeholder={field.label} />
+                </div>
+              )
+            }
+
+            if (field.type == 'select') {
+              return (
+                <div className="sel" key={`field-${i}`}>
+                  <i className={`fa ${field.icon}`}></i>
+                  <select id={field.id}>
+                    <option value=''>{field.label}</option>
+                    {field.options.map((option, i) => (
+                      <option key={`option-${i}`} value={option.value}>{option.label}</option>
+                    ))}
+                  </select>
+                </div>
+              )
+            }
+
+          })}
+
         <h2><button type="submit">Submit Form</button></h2>
+
         </form>
       </div>
     )
